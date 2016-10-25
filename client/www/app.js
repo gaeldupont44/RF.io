dccb501dca39bb86520a1d551d21afefa225ec97
Gaël Dupont <gaeldupont44@ns370490.ip-91-121-86.eu>
2016-10-25 23:57:59 +0200

    fix webSQL incompatibility

--- client/www/app.js
+++ client/www/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('RFio', ['ionic', 'angularjs-dropdown-multiselect', 'btford.socket-io', 'RFio.bridges', 'RFio.receivers', 'RFio.rooms'])
+angular.module('RFio', ['ionic', 'angularjs-dropdown-multiselect', 'angular-websql', 'btford.socket-io', 'RFio.bridges', 'RFio.receivers', 'RFio.rooms'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
