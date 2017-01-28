ef96205eb819c9d20696c3facf205a097b04106c
Gaël Dupont <gaeldupont44@ns370490.ip-91-121-86.eu>
2017-01-28 10:37:20 +0100

    picture upload improved

--- api/picture/index.js
+++ api/picture/index.js
@@ -14,36 +14,6 @@ var routes = [
 		    handler: controller.create,
 		    config: {
 		        payload: {
-		            parse: false,
-		            maxBytes: 50 * 1024 * 1024
-		        }
-		    }
-		},
-		{
-		    method: 'DELETE',
-		    path: '/api/picture/{id}',
-		    handler: controller.delete
-		}
-	];
-
-module.exports = routes;
-
-'use strict';
-
-var controller = require('./picture.controller');
-
-var routes = [
-		{
-	        method: 'GET',
-	    	path: '/api/picture/{id}',
-	    	handler: controller.get
-		},
-		{
-		    method: 'POST',
-		    path: '/api/picture',
-		    handler: controller.create,
-		    config: {
-		        payload: {
 		        	timeout: 30000,
 		        	maxBytes: 50 * 1024 * 1024,
 		            parse: true,
